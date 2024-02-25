8f5422700161d851f39b7d0add3a215051a434b2
deriverge <158823782+deriverge@users.noreply.github.com>
2024-02-25 13:46:05 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function menuClicked(){
+const menuButton = document.getElementById("menu-mobile-button");
+if (menuButton.innerText === "Zobrazit menu"){
+menuButton.textContent = "Zavřít menu";
+
+} else {
+    menuButton.textContent = "Zobrazit menu";
+
+}
+}
+
+function menuButtonClicked(){
+const colorButton = document.getElementById("menu-mobile-button-container");
+const menuButton = document.getElementById("menu-mobile-button");
+if (menuButton.innerText === "Zobrazit menu"){
+colorButton.style.backgroundColor = "rgba(6,6,68, 0.9)";
+} else {
+    colorButton.style.backgroundColor = "rgba(33, 33, 33, 0.9)";
+}
+}
+
+function showMobileMenu(){
+const menuButton = document.getElementById("menu-mobile-button");
+const showMobileMenu = document.getElementById("menu-mobile");
+if (menuButton.innerText === "Zobrazit menu"){
+showMobileMenu.style.display = "none";
+} else {
+showMobileMenu.style.display = "flex";
+}
+}
